ad4bc51d74a26b7bbeca614077a4e178e7aafddf
nkm90 <nkm_90@hotmail.com>
2020-05-11 21:02:50 +0100

    The icon for the app has been implemented and the final password to check too

--- app/src/main/java/com/example/breakscape/BreakActivity.java
+++ app/src/main/java/com/example/breakscape/BreakActivity.java
@@ -4,13 +4,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BreakActivity extends Timer {
+    private LinearLayout inputArea;
+    String inputs;
+    //This
+    EditText num1;
+    EditText num2;
+    EditText num3;
+    EditText num4;
+    private String code = "7044";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_break);
+
+        num1 = findViewById(R.id.num1);
+        num2 = findViewById(R.id.num2);
+        num3 = findViewById(R.id.num3);
+        num4 = findViewById(R.id.num4);
+
 
         Button check = (Button) findViewById(R.id.enter);
         check.setOnClickListener(new View.OnClickListener() {
@@ -30,9 +50,20 @@ public class BreakActivity extends Timer {
     }
 
     public void check(View view){
-        //if the answer provided is correct, congratulations activity will appear
-        Intent intent = new Intent(this, CongratulationsActivity.class);
-        startActivity(intent);
+        String input1 = num1.getText().toString();
+        String input2 = num2.getText().toString();
+        String input3 = num3.getText().toString();
+        String input4 = num4.getText().toString();
+
+        if(input1.equals("7") && input2.equals("0") && input3.equals("4") && input4.equals("4")){
+            //if the answer provided is correct, congratulations activity will appear
+            Intent intent = new Intent(this, CongratulationsActivity.class);
+            startActivity(intent);
+            this.finish();
+        }else {
+
+        }
+
     }
     public void home(View view){
         Intent intent = new Intent(this, MenuActivity.class);
